8b86762653585c70de1d428dbc60690aa4bf989b
Lucas Haug <lucas.haug@ipvs.uni-stuttgart.de>
2025-02-07 22:10:11 +0100

    Small fixes

--- src/gptp/DetComGptp.h
+++ src/gptp/DetComGptp.h
@@ -38,7 +38,7 @@ class DetComGptp : public InterfaceFilterMixin<Gptp>
   protected:
     void initialize(int stage) override;
     virtual void processFollowUp(Packet *packet, const GptpFollowUp *gptp) override;
-    virtual void processSync(Packet *packet, const GptpSync *gptp);
+    virtual void processSync(Packet *packet, const GptpSync *gptp) override;
     virtual void sendFollowUp(int portId, const GptpSync *sync, const clocktime_t &syncEgressTimestampOwn) override;
     virtual void synchronize() override;
     virtual void sendSync() override;
