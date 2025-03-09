13e89e7fd700fef45d21ff07b55e993217fe1f4f
DanYuDE <nadal3815@gmail.com>
2025-03-09 11:35:08 +0100

    Revise header comments

--- src/devices/tsntranslator/TTPacketDirectionReverser.h
+++ src/devices/tsntranslator/TTPacketDirectionReverser.h
@@ -1,6 +1,9 @@
+// This file is part of Deliverable D4.4 [D44PLACEHOLDER]
+// of the DETERMINISTIC6G project receiving funding from the
+// European Union’s Horizon Europe research and innovation programme
+// under Grant Agreement No. 101096504.
 //
-// Created by haugls on 9/3/24.
-//
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
 #ifndef DETERMINISTIC6G_TTPACKETDIRECTIONREVERSER_H
 #define DETERMINISTIC6G_TTPACKETDIRECTIONREVERSER_H
--- src/timestamping/TimeChunkDissector.h
+++ src/timestamping/TimeChunkDissector.h
@@ -1,8 +1,9 @@
-//
-// Copyright (C) 2018 OpenSim Ltd.
+// This file is part of Deliverable D4.4 [D44PLACEHOLDER]
+// of the DETERMINISTIC6G project receiving funding from the
+// European Union’s Horizon Europe research and innovation programme
+// under Grant Agreement No. 101096504.
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
-//
 
 #ifndef __INET_IEEE8021QTAGEPDPROTOCOLDISSECTOR_H
 #define __INET_IEEE8021QTAGEPDPROTOCOLDISSECTOR_H
