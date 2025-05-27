2383d8e0731938a09aaf67cbce543d751fb7f59f
Lucas Haug <lucas.haug@ipvs.uni-stuttgart.de>
2025-05-27 16:39:56 +0200

    Changed license headers and updated documentation.yml

--- src/gptp/DetComGptp.cc
+++ src/gptp/DetComGptp.cc
@@ -1,3 +1,10 @@
+// This file is part of Deliverable D4.4 DetCom Simulator Framework Release 2
+// of the DETERMINISTIC6G project receiving funding from the
+// European Union’s Horizon Europe research and innovation programme
+// under Grant Agreement No. 101096504.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 #include "DetComGptp.h"
 
 #include "../timestamping/DetComTimeTag_m.h"
